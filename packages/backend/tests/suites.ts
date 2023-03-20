2295ec96cec9e734e0a19e888f6c0e573826454d
panteliselef <panteliselef@outlook.com>
2023-03-20 15:06:38 +0100

    chore(backend): Use helper functions to construct interstitial script
    The first tests for the interstitial scripts are introduced by this commit. This allows us to test specific parts for the interstitial script that we are creating. We want to ensure that we are passing the correct parameters with the correct format to clerk-js.

--- packages/backend/tests/suites.ts
+++ packages/backend/tests/suites.ts
@@ -4,6 +4,7 @@ import apiTest from './dist/api/factory.test.js';
 import requestTest from './dist/tokens/request.test.js';
 import keysTest from './dist/tokens/keys.test.js';
 import pathTest from './dist/util/path.test.js';
+import interstitialTest from './dist/tokens/interstitial.test.js';
 import verifyTest from './dist/tokens/verify.test.js';
 import jwtTest from './dist/tokens/jwt.test.js';
 
@@ -11,6 +12,16 @@ import utilRequestTest from './dist/util/request.test.js';
 import factoryTest from './dist/api/factory.test.js';
 
 // Add them to the suite array
-const suites = [apiTest, requestTest, utilRequestTest, keysTest, verifyTest, pathTest, jwtTest, factoryTest];
+const suites = [
+  apiTest,
+  requestTest,
+  utilRequestTest,
+  keysTest,
+  verifyTest,
+  pathTest,
+  interstitialTest,
+  jwtTest,
+  factoryTest,
+];
 
 export default suites;
